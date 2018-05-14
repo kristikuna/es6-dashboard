2f231bb3c43c3628f166c7622b7c4bc08aefd6ef
ONEDB85 <dloreanbell@gmail.com>
2018-05-14 16:14:02 -0400

    getData function

--- src/_modules/to-do/to-do.js
+++ src/_modules/to-do/to-do.js
@@ -2,31 +2,29 @@
 
 export default class ToDo {
   constructor() {
+    const url = 'http://localhost:4000/tasks';
     this.name = 'to-do';
     console.log('%s module', this.name.toLowerCase());
+    this.getData(url);
+
+    
+    const item = document.getElementById("list");
   }
+  getData(url) {
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data[1].title)
+      })
+      .catch(error => {
+        console.error('Error:', error)
+      });
+  };
+ 
 }
 
-const url = 'https://localhost:4000/db.json';
-console.log(url);
-// const data = /db.json;
-// console.log(data);
 
-fetch(url, {
-  method: 'GET', // or 'PUT'
-  body: JSON.stringify(data), // data can be `string` or {object}!
-  headers: new Headers({
-    'Content-Type': 'application/json'
-  })
-}).then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => console.log('Success:', response));
 
-// fetch(url)
-// .then( (response)=> {
-//   return response.json();
-//   console.log(response);
-// })
-// .then(function(myJson) {
-//   console.log(myJson);
-// });
+
+
